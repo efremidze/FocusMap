fe7a453e8bd3f729483e69da2a32d44eacecee0b
Lasha Efremidze <efremidzel@hotmail.com>
2015-03-30 00:54:21 -0700

    Initial Commit

---
+++ FocusMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FocusMap
+//
+//  Created by Lasha Efremidze on 3/30/15.
+//  Copyright (c) 2015 More Voltage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FocusMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FocusMap
+//
+//  Created by Lasha Efremidze on 3/30/15.
+//  Copyright (c) 2015 More Voltage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
